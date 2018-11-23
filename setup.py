b63220fd4ad33b46535a2d3eed295fcace5c818c
xff <sailbafan@gmail.com>
2018-11-23 17:09:56 +0800

    add a required library

--- setup.py
+++ setup.py
@@ -6,13 +6,9 @@ assert sys.version_info.major == 3 and sys.version_info.minor >= 6, \
     "The Spinning Up repo is designed to work with Python 3.6 and greater." \
     + "Please install it before proceeding."
 
-with open(join("spinup", "version.py")) as version_file:
-    exec(version_file.read())
-
 setup(
     name='spinup',
     py_modules=['spinup'],
-    version=__version__,#'0.1',
     install_requires=[
         'cloudpickle==0.5.2',
         'gym[atari,box2d,classic_control]>=0.10.8',
@@ -27,7 +23,8 @@ setup(
         'scipy',
         'seaborn==0.8.1',
         'tensorflow>=1.8.0',
-        'tqdm'
+        'tqdm',
+        'gin-config'
     ],
     description="Teaching tools for introducing people to deep RL.",
     author="Joshua Achiam",

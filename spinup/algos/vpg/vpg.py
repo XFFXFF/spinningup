4f8c15bffde2de3665cede1549309c68a0057bae
xff <sailbafan@gmail.com>
2018-12-10 16:45:52 +0800

    load the gin_files in __main__

--- spinup/algos/vpg/vpg.py
+++ spinup/algos/vpg/vpg.py
@@ -367,6 +367,8 @@ if __name__ == '__main__':
     parser.add_argument('--seed', '-s', type=int, default=0)
     parser.add_argument('--cpu', type=int, default=2)
     parser.add_argument('--exp_name', type=str, default='vpg')
+    parser.add_argument('--gin_files', nargs='+', default=["vpg.gin"])
+    parser.add_argument('--gin_bindings', nargs='+', default=[])
     parser.add_argument('--test', action='store_true')
     
     args = parser.parse_args()
@@ -376,6 +378,7 @@ if __name__ == '__main__':
     from spinup.utils.run_utils import setup_logger_kwargs
     logger_kwargs = setup_logger_kwargs(exp_name=args.exp_name, env_name=args.env, seed=args.seed)
 
+    load_gin_configs(args.gin_files, args.gin_bindings)
     tf.logging.set_verbosity(tf.logging.INFO)
     runner = VPGRunner(args.env, args.seed, args.epochs, logger_kwargs=logger_kwargs)
     if args.test:

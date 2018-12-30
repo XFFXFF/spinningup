ba987dfd77368853da15ec70c3ac4c167b5d2418
xff <sailbafan@gmail.com>
2018-12-30 09:54:01 +0800

    Made it better

--- spinup/algos/sac.py
+++ spinup/algos/sac.py
@@ -14,9 +14,9 @@ LOG_STD_MAX = 2
 LOG_STD_MIN = -20
 
 
-class TD3Buffer(object):
+class SACBuffer(object):
     """
-    A simple FIFO experience replay buffer for TD3 agents.
+    A simple FIFO experience replay buffer for SAC agents.
     """
 
     def __init__(self, obs_dim, act_dim, size):
@@ -41,7 +41,7 @@ class TD3Buffer(object):
         return self.obs_buf[idxs], self.acts_buf[idxs], self.rews_buf[idxs], self.next_obs_buf[idxs], self.done_buf[idxs]
 
 
-class TD3Net(object):
+class SACNet(object):
     
     def __init__(self,
                  obs,
@@ -110,15 +110,15 @@ class TD3Net(object):
         return self.pi, self.q1, self.q1_pi, self.q2, self.q2_pi, self.v, self.logp_pi
         
 
-class TD3Agent(object):
-    """An implementation of TD3 agent."""
+class SACAgent(object):
+    """An implementation of SAC agent."""
 
     def __init__(self,
                  obs_dim,
                  act_dim,
                  act_space_high,
                  gamma=0.99,
-                 alpha=0.1,
+                 alpha=0.2,
                  target_noise=0.2,
                  noise_clip=0.5,
                  q_lr=0.001,
@@ -132,7 +132,9 @@ class TD3Agent(object):
             obs_dim: int, The dimensions of obs vector.
             act_dim: int, The dimensions of act vector.
             act_space_high: float, The maximum value act can take.
-            gamma: float,  Discount factor. (Always between 0 and 1.)
+            gamma: float, Discount factor. (Always between 0 and 1.)
+            alpha: float, Entropy regularization coefficient. (Equivalent to 
+                inverse of reward scale in the original SAC paper.)
             target_noise: float, Stddev for smoothing noise added to target 
                 policy.
             noise_clip: float, Limit for absolute value of target policy 
@@ -170,11 +172,11 @@ class TD3Agent(object):
         self.q_loss = q1_loss + q2_loss
         self.train_q_op = tf.train.AdamOptimizer(q_lr).minimize(self.q_loss)
 
-        v_loss = tf.reduce_mean((self.v - y_v)**2)
-        self.train_v_op = tf.train.AdamOptimizer(v_lr).minimize(v_loss)
+        self.v_loss = tf.reduce_mean((self.v - y_v)**2)
+        self.train_v_op = tf.train.AdamOptimizer(v_lr).minimize(self.v_loss)
 
         pi_params = self._get_vars('main/pi')
-        self.pi_loss = tf.reduce_mean(-self.q1_pi - alpha * self.logp_pi)
+        self.pi_loss = tf.reduce_mean(-self.q1_pi + alpha * self.logp_pi)
         self.train_pi_op = tf.train.AdamOptimizer(pi_lr).minimize(self.pi_loss, var_list=pi_params)
 
         main_params = self._get_vars('main')
@@ -201,10 +203,10 @@ class TD3Agent(object):
 
     def _create_network(self):
         with tf.variable_scope('main'):
-            main_net = TD3Net(self.obs_ph, self.act_ph, self.act_dim, self.act_space_high)
+            main_net = SACNet(self.obs_ph, self.act_ph, self.act_dim, self.act_space_high)
             self.pi, self.q1, self.q1_pi, self.q2, self.q2_pi, self.v, self.logp_pi = main_net.network_output()
         with tf.variable_scope('target'):
-            target_net = TD3Net(self.next_obs_ph, self.act_ph, self.act_dim, self.act_space_high)
+            target_net = SACNet(self.next_obs_ph, self.act_ph, self.act_dim, self.act_space_high)
             _, _, _, _, _, self.v_targ, _ = target_net.network_output()
     
     def update_q(self, feed_dict):
@@ -212,7 +214,8 @@ class TD3Agent(object):
         return q_loss
 
     def update_v(self, feed_dict):
-        self.sess.run(self.train_v_op, feed_dict=feed_dict)
+        _, v_loss = self.sess.run([self.train_v_op, self.v_loss], feed_dict=feed_dict)
+        return v_loss
 
     def update_pi(self, feed_dict):
         _, pi_loss = self.sess.run([self.train_pi_op, self.pi_loss], feed_dict=feed_dict)
@@ -226,7 +229,7 @@ class TD3Agent(object):
         return act
 
 
-class TD3Runner(object):
+class SACRunner(object):
 
     def __init__(self,
                  env, 
@@ -234,11 +237,10 @@ class TD3Runner(object):
                  epochs=50,
                  train_epoch_len=5000,
                  test_epoch_len=2000,
-                 random_act=1000,
-                 batch_size=32,
+                 random_act=10000,
+                 batch_size=100,
                  buffer_size=int(1e6),
                  act_noise=0.1,
-                 policy_delay=1,
                  logger_kwargs=dict()):
         """Initialize the Runner object.
 
@@ -267,7 +269,6 @@ class TD3Runner(object):
         tf.logging.info(f'\t batch_size: {batch_size}')
         tf.logging.info(f'\t buffer_size: {buffer_size}')
         tf.logging.info(f'\t act_noise: {act_noise}')
-        tf.logging.info(f'\t policy_delay: {policy_delay}')
         self.env = gym.make(env)
         self.epochs = epochs
         self.train_epoch_len = train_epoch_len
@@ -275,7 +276,6 @@ class TD3Runner(object):
         self.random_act = random_act
         self.batch_size = batch_size
         self.act_noise = act_noise
-        self.policy_delay = policy_delay
         self.logger_kwargs = logger_kwargs
         self.max_ep_len = self.env.spec.timestep_limit
 
@@ -286,8 +286,8 @@ class TD3Runner(object):
         obs_dim = self.env.observation_space.shape[0]
         act_dim = self.env.action_space.shape[0]
         act_space_high = self.env.action_space.high
-        self.agent = TD3Agent(obs_dim, act_dim, act_space_high)
-        self.buffer = TD3Buffer(obs_dim, act_dim, buffer_size)
+        self.agent = SACAgent(obs_dim, act_dim, act_space_high)
+        self.buffer = SACBuffer(obs_dim, act_dim, buffer_size)
 
     def _run_train_phase(self, logger):
         ep_r, ep_len = 0, 0
@@ -307,26 +307,29 @@ class TD3Runner(object):
             
             if done or ep_len == self.max_ep_len:
                 """
-                Perform all TD3 updates at the end of the trajectory
-                (in accordance with source code of TD3 published by
+                Perform all SAC updates at the end of the trajectory
+                (in accordance with source code of SAC published by
                 original authors).
                 """
-                if not self.random_act:
-                    for i in range(ep_len):
-                        obs_buf, act_buf, rew_buf, next_obs_buf, done_buf = self.buffer.sample_batch(self.batch_size)
-                        feed_dict = {
-                            self.agent.obs_ph: obs_buf,
-                            self.agent.act_ph: act_buf,
-                            self.agent.rew_ph: rew_buf,
-                            self.agent.next_obs_ph: next_obs_buf,
-                            self.agent.done_ph: done_buf,
-                        }
-                        q_loss = self.agent.update_q(feed_dict)
-                        logger.store(QLoss=q_loss)
-                        self.agent.update_v(feed_dict)
-                        pi_loss = self.agent.update_pi(feed_dict)
-                        logger.store(PiLoss=pi_loss)
-                        self.agent.update_target()
+                for i in range(ep_len):
+                    obs_buf, act_buf, rew_buf, next_obs_buf, done_buf = self.buffer.sample_batch(self.batch_size)
+                    feed_dict = {
+                        self.agent.obs_ph: obs_buf,
+                        self.agent.act_ph: act_buf,
+                        self.agent.rew_ph: rew_buf,
+                        self.agent.next_obs_ph: next_obs_buf,
+                        self.agent.done_ph: done_buf,
+                    }
+                    q_loss = self.agent.update_q(feed_dict)
+                    logger.store(QLoss=q_loss)
+
+                    v_loss = self.agent.update_v(feed_dict)
+                    logger.store(VLoss=v_loss)
+
+                    pi_loss = self.agent.update_pi(feed_dict)
+                    logger.store(PiLoss=pi_loss)
+
+                    self.agent.update_target()
                 logger.store(EpRet=ep_r, EpLen=ep_len)
                 obs = self.env.reset()
                 ep_r, ep_len = 0, 0
@@ -359,6 +362,7 @@ class TD3Runner(object):
             logger.log_tabular('TestEpRet', with_min_and_max=True)
             logger.log_tabular('TestEpLen', average_only=True)
             logger.log_tabular('QLoss', average_only=True)
+            logger.log_tabular('VLoss', average_only=True)
             logger.log_tabular('PiLoss', average_only=True)
             logger.log_tabular('TotalEnvInteracts', (epoch + 1) * self.train_epoch_len)
             logger.log_tabular('Time', time.time() - start_time)
@@ -368,7 +372,7 @@ class TD3Runner(object):
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env', type=str, default='Pendulum-v0')
+    parser.add_argument('--env', type=str, default='HalfCheetah-v2')
     parser.add_argument('--seed', '-s', type=int, default=0)
     parser.add_argument('--epochs', type=int, default=50)
     parser.add_argument('--extra_exp_name', '-e', type=str, default='')
@@ -378,6 +382,6 @@ if __name__ == '__main__':
     logger_kwargs = setup_logger_kwargs('sac', args.env, args.seed, extra_exp_name=args.extra_exp_name)
 
     tf.logging.set_verbosity(tf.logging.INFO)
-    runner = TD3Runner(env=args.env, seed=args.seed, epochs=args.epochs, logger_kwargs=logger_kwargs)
+    runner = SACRunner(env=args.env, seed=args.seed, epochs=args.epochs, logger_kwargs=logger_kwargs)
     runner.run_experiment()
     
